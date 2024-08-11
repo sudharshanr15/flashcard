8e90ea1218c100a7eb54035c425e9bddb8135121
Sudharshan <sudharshanrms2004@gmail.com>
2024-08-11 23:12:28 +0530

    Added swiper for flash cards

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,5 +1,14 @@
 import React, { useRef } from 'react'
 
+const questionColors = [
+    // 'bg-amber-600',
+    // 'bg-yellow-600',
+    // 'bg-sky-600',
+    'bg-blue-600',
+    // 'bg-indigo-600',
+    // 'bg-violet-600'
+]
+
 const FlashCard = () => {
     const flashcard_container_ref = useRef();
 
@@ -7,14 +16,18 @@ const FlashCard = () => {
         // document.querySelector(".flashcard-container").classList.add("rotate-y-180")
         flashcard_container_ref.current.classList.add("rotate-y-180")
     }
+
+    const questionColor = questionColors[Math.round(Math.random() * 10) % questionColors.length]
     return (
-        <div className="flashcard-wrapper" onClick={revealAnswer}>
-            <div className="w-[250px] h-[500px] flex items-center justify-center border rounded-md flashcard-container relative" ref={flashcard_container_ref}>
-                <div className="bg-red-900 flashcard-question m-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci nobis, consequatur maiores libero et qui ut corrupti iusto vitae.
-                </div>
-                <div className="bg-green-900 flashcard-answer m-4">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ab id corrupti? Corrupti harum, eum fugit aliquam magnam tempore mollitia.
+        <div className="flex items-center justify-center">
+            <div className="flashcard-wrapper" onClick={revealAnswer}>
+                <div className="w-[250px] h-[500px] flex items-center justify-center border border-gray-500 rounded-md flashcard-container relative" ref={flashcard_container_ref}>
+                    <div className={`${questionColor} flashcard-question h-full p-4`}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci nobis, consequatur maiores libero et qui ut corrupti iusto vitae.
+                    </div>
+                    <div className={`bg-lime-400 flashcard-answer h-full p-4 text-black`}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ab id corrupti? Corrupti harum, eum fugit aliquam magnam tempore mollitia.
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Topic.jsx
+++ src/pages/Topic.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import FlashCard from '../components/FlashCard'
+// import Swiper from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const topics = [
     {
@@ -30,10 +36,30 @@ const Topic = ({ id }) => {
     const params = useParams()
 
     return (
-        <>
-        <FlashCard />
-        <FlashCard />
-        </>
+        // <>
+        // <FlashCard />
+        // <FlashCard />
+        // </>
+        <Swiper
+            slidesPerView={1}
+            loop={false}
+            navigation={true}
+            modules={[Navigation]}
+            draggable={false}
+            keyboard={true}
+            mousewheel={true}
+            allowTouchMove={false}
+        >
+            <SwiperSlide>
+                <FlashCard />
+            </SwiperSlide>
+            <SwiperSlide>
+                <FlashCard />
+            </SwiperSlide>
+            <SwiperSlide>
+                <FlashCard />
+            </SwiperSlide>
+        </Swiper>
     )
 }
 
