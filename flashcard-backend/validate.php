712c4232c742c2b758ccfd2acdc0c51decf50ed6
Sudharshan <sudharshanrms2004@gmail.com>
2024-08-13 17:07:02 +0530

    Added token checkpoint

--- flashcard-backend/validate.php
+++ flashcard-backend/validate.php
@@ -1,13 +1,23 @@
 <?php
 
-require_once __DIR__ . "/conn.php";
+// require_once __DIR__ . "/conn.php";
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: https://sudharshan.selfmade.one");
 header("Access-Control-Allow-Credentials: true");
 
 
-$token = $_COOKIE['access_token'];
+$token = $_COOKIE['access_token'] ?? null;
+
+if(!$token){
+    $res = [
+        "status" => false,
+        "message" => "Authentication required"
+    ];
+    http_response_code(401);
+
+    die(json_encode($res));
+}
 
 try{
     $query = $conn->prepare("SELECT * from access_tokens where token=:token ORDER BY id DESC LIMIT 1");
